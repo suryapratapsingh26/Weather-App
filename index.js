c9105a925dc6010788b5f220647eec26be149c1d
Suryapratapsingh26 <146753809+Suryapratapsingh26@users.noreply.github.com>
2023-12-24 23:18:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ search.addEventListener('click', () => {
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
+            container.style.height = '600px';
 
 
         });
